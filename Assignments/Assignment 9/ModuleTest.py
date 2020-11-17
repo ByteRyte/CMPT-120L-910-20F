b4914effe7e9abb16d73aa6a2df7ababc1eeacdb
ByteRyte <andrew.riotto1@marist.edu>
2020-11-17 16:00:43 -0500

    Completed Assignment #9

---
+++ Assignments/Assignment 9/ModuleTest.py
@@ -0,0 +1,11 @@
+import Calculator
+
+MathMachine = Calculator.Calculator()
+
+print(MathMachine.addition(5, 3))
+print(MathMachine.subtraction(7, 4))
+print(MathMachine.division(9, 3))
+print(MathMachine.multiplication(5, 5))
+print(MathMachine.exponent(9, 3))
+print(MathMachine.square_root(125))
+print(MathMachine.negate(-7))
