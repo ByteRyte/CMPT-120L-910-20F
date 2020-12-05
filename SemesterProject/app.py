f5004447eecbafdc25f7a02ffecdfcbf09fb04c2
ByteRyte <andrew.riotto1@marist.edu>
2020-12-04 22:32:04 -0500

    Update app.py

--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -2,13 +2,19 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+app = Flask(__name__)
+
+##app.logger.setLevel(logging.INFO)
+
 @app.route('/')
 def hello_world():
+    app.logger.info("User traveled to the info route.")
     return 'Hello, World!'
 
-Hello = Hello
+
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name = None):
+    app.logger.warning("This page isn't finished yet but we say hello.")
     return render_template('hello.html', name=name)
